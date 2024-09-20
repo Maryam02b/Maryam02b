fa48027d7ad0f8cecc5963cc0bd6f3f9b3ce444a
Maryam02b <maryambadr02a@gmail.com>
2024-09-20 20:44:39 +0330

    Create M.m

---
+++ M.m
@@ -0,0 +1,27 @@
+const num1=[30.0,18,16,24,15,74,-8];
+const num2=[18+12,6-2,4*8,62/12,88];
+const num3=[X+3,y-5,28,82,12.18];
+
+const result1=num3.indexof(0)=num1.indexof(2);
+console.log(result1);
+
+const result2=num3.indexof(1)=num2.indexof(3);
+console.log(result2);
+
+const result3of num2=num2.map((item=>{
+    if(item=num1.indexof(0)){
+        return item;
+    }else{
+        return item/2
+    }
+}))
+console.log(result3of num2);
+
+const result4 of num1=num1.map((item=>{
+    if(item>num2.indexof(4)){
+        return item;
+    }else{
+        return*2
+    }
+}))
+console.log(result4 of num);
